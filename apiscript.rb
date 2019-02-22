5a679d269163cdc8bb253b26aaa9415c5ed9bfa4
mccarthy417nahant <kevinmccarthy01@comcast.net>
2019-02-22 09:34:00 -0500

    comment out pry

--- apiscript.rb
+++ apiscript.rb
@@ -17,9 +17,9 @@ class PrecisionLocation
       :body => "<LocationRQ xmlns=\"http://skyhookwireless.com/wps/2005\" version=\"2.26\" street-address-lookup=\"full\"><authentication version=\"2.2\"><key key=\"eJwz5DQ0AAELM3NTzmpXQyMnR1dLY11zEzNHXVcXE1ddIM9S19HN3MnQyM3N0tXVtRYAF1ELjA\" username=\"C02W503XG8WN\" /></authentication>
         <access-point><mac>6C96CFE0303D</mac><ssid>Lucy's House</ssid><signal-strength>-36</signal-strength></access-point>
       </LocationRQ>")
-    require "pry"
-    binding.pry
-    response
+    # require "pry"
+    # binding.pry
+    # response
   end
 end
 
